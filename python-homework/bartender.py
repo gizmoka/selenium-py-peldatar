63526a10b4349a98cf5616b75408fd3bbeea6793
Gizella Grunda-Hibaly <gizella.grundahibaly@gmail.com>
2021-06-22 15:47:25 +0200

    Description added.

--- python-homework/bartender.py
+++ python-homework/bartender.py
@@ -1,6 +1,13 @@
 # selenium-py/001-python-alapok-1
 # 28. feladat: Vezérlési szerkezetek gyakorlása.
 
+# Kérd be a felhasználó életkorát, és kérdezd meg, mit iszik.
+# Kétféle italt ismerünk: sör és kóla.
+# Ha a felhasználó kiskorú, és sört kér, akkor közöld vele, hogy sajnos nem adhatsz neki.
+# Ha a felhasználó 60 feletti, és kólát kér, akkor közöld vele, hogy a koffein megemelheti a vérnyomását.
+# Ha ismeretlen italt adott meg, akkor írd ki, hogy csak sört és kólát tudunk adni.
+# Minden más esetben szolgáld ki. (Írd ki pl. "Parancsoljon, a söre." vagy "Parancsoljon,a kólája.")
+
 age = int(input("Életkorod? "))
 drink = input("Mit iszol? ")
 if age < 18 and drink == "sör":
