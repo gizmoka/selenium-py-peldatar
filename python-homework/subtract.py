62c70e691b0323d1b5258e40df4e9d49d4c93f8e
Gizella Grunda-Hibaly <gizella.grundahibaly@gmail.com>
2021-06-22 15:41:16 +0200

    subtraction finalised.

---
+++ python-homework/subtract.py
@@ -0,0 +1,6 @@
+# selenium-py/001-python-alapok-1
+# 16. feladat: Python Adatok és változók feladat. 1. alfeladat
+
+a = int(input("Adj meg egy számot! "))
+b = int(input("Adj meg egy másik számot! És én kiszámolom a kettő különbségét. Mi a számod? "))
+print(f"A különbségük: {a - b}.")
