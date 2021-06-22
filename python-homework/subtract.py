9cfe2675e1a047588c4acac51d67d189c90eb932
Gizella Grunda-Hibaly <gizella.grundahibaly@gmail.com>
2021-06-22 15:45:52 +0200

    Description added.

--- python-homework/subtract.py
+++ python-homework/subtract.py
@@ -1,6 +1,9 @@
 # selenium-py/001-python-alapok-1
 # 16. feladat: Python Adatok és változók feladat. 1. alfeladat
 
+# Kérjünk be két számot, és írjuk ki a különgségüket.
+# Használd az input() és print() beépített függvényeket.
+
 a = int(input("Adj meg egy számot! "))
 b = int(input("Adj meg egy másik számot! És én kiszámolom a kettő különbségét. Mi a számod? "))
 print(f"A különbségük: {a - b}.")
