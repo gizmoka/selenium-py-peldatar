7c37bc556d22ddb1e019cc912687215643966e7d
Gizella Grunda-Hibaly <gizella.grundahibaly@gmail.com>
2021-06-22 16:11:05 +0200

    Description added.

---
+++ python-homework/charsandord.py
@@ -0,0 +1,13 @@
+# selenium-py/001-python-alapok-1
+# 22 Feladat: Python for ciklus gyakorlása.
+
+Írj programot, ami kiírja a kisbetűket, és melléjük az karakter kódjukat!
+A kiírás több oszlopos legyen, és legfeljebb 10 soros.
+Minta:
+ a 97 f 102 .....
+ b 98 g 103
+ c 99 h 104
+ d 100 i 105
+ e 101 j 106
+
+A megoldashoz használd a beépített ord() es chr() függvényeket.
