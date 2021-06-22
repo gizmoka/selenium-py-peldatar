727c71c834d2282508eb79ec3d8550628636ee54
Gizella Grunda-Hibaly <gizella.grundahibaly@gmail.com>
2021-06-22 16:10:55 +0200

    Description added.

---
+++ python-homework/tenbelow.py
@@ -0,0 +1,6 @@
+# selenium-py/001-python-alapok-1
+# 32 Feladat: Python while ciklus gyakorlása.
+
+# Írj programot, mely addig olvas be számokat a billentyűzetről, ameddig azok kisebbek, mint tíz.
+# Írja ki ezek után a beolvasott számok összegét!
+
