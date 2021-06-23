fa066a152c05ce42252b32496db29a319f43991b
Gizella Grunda-Hibaly <gizella.grundahibaly@gmail.com>
2021-06-23 21:50:54 +0200

    Description added.

---
+++ selenium-homework/locators.py
@@ -0,0 +1,13 @@
+# selenium-py/002-python-selenium-alapok-1
+# 013 Feladat: Lokátorok gyakorlása
+
+# Készíts egy Python alkalmazást ami selenium-ot használ.
+# Indítsd el lokálisan a selenium-py-peldatar alkalmazást.
+# A program töltse be a példatárból az http://localhost:9999/kitchensink.html oldalt.
+# Gyakorlás képpen keress ki elemekt a képernyőről az alábbi kritériumoknak megfelelően:
+# - ID alapján
+# - NAME alapján
+# - XPath kifejezéssel Minden megtalált elemnek irassd ki a text értékét, vagy egy attribútum értékét.
+# A megoldást egy locators.py nevű fileban kell beadnod.
+
+
