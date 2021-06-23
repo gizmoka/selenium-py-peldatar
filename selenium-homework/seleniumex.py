9df477f26f0f279c887ee0c123ac637ea1a02e3d
Gizella Grunda-Hibaly <gizella.grundahibaly@gmail.com>
2021-06-23 21:43:31 +0200

    Description added.

---
+++ selenium-homework/seleniumex.py
@@ -0,0 +1,6 @@
+# selenium-py/002-python-selenium-alapok-1
+# 008 Feladat: Hibakezelés megvalósítása selenium programban
+
+# Készíts egy Python alkalmazást ami selenium-ot használ. Nyisson meg egy Chrome böngészöt és töltsön be egy tetszőleges weblapot az Internetről. Az oldalon probáld megtalálni a <div id="nemletezik"></div> mezőt. A lényeg, hogy hibát dogjon a driver.find_by_id függvény hívás. Feladatot, hogy kezed le ezt a hibát és írj ki valami emberileg olvasható üzenetet. Extra feladatként készíts egy saját függvényt, ami bármilyen find_by_id lokátor hívásnál lekezeli a nemlétező elem tipusú hibát.
+# A megoldást egy seleniumex.py nevű fileban kell beadnod.
+
