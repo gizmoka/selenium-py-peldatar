230682fbd8564f702e280a22a0a0628a4233d880
Gizella Grunda-Hibaly <gizella.grundahibaly@gmail.com>
2021-06-23 21:14:11 +0200

    1 variable added.

---
+++ selenium-homework/bmodul.py
@@ -0,0 +1,7 @@
+# selenium-py/002-python-selenium-alapok-1
+# 003 Feladat: Python modulok gyakorlása
+# Készíts egy bmodul nevezetű python modult. Hozz benne létre egy változót és tárol benne egy értéket. Egy másik modulban, mondjuk main modul, importáld be a bmodul-ból a változót és használd fel egy kiiratásban.
+# A megoldást egy bmodul.py és egy main.py nevű fileban kell beadnod.
+
+welcome = "hello"
+
