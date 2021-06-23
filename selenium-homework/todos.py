460ce06291704564e63f173879b6971045cceee7
Gizella Grunda-Hibaly <gizella.grundahibaly@gmail.com>
2021-06-23 21:50:37 +0200

    Description added.

---
+++ selenium-homework/todos.py
@@ -0,0 +1,11 @@
+# selenium-py/002-python-selenium-alapok-1
+# 015 Feladat: Selenium adat kinyeréses feladatok
+
+# Készíts egy Python alkalmazást ami selenium-ot használ.
+# Indítsd el lokálisan a selenium-py-peldatar alkalmazást. A
+# program töltse be a példatárból az http://localhost:9999/todo.html oldalt.
+# A feladatod, hogy kigyűjtsd az összes jelenleg aktív Todo bejegyzést.
+# Ha lehet akkor ezt minnél kevesebb selenium lokátorral old meg.
+# (Tökéletes megoldáshoz csak egy darab find_by hívásra van szükség).
+# A megoldást egy todos.py nevű fileban kell beadnod.
+
