931c2db4859fdcbf6b2656b0af0e52caf6808aec
Gizella Grunda-Hibaly <gizella.grundahibaly@gmail.com>
2021-06-23 21:45:42 +0200

    Description added.

---
+++ selenium-homework/elementtype.py
@@ -0,0 +1,11 @@
+# selenium-py/002-python-selenium-alapok-1
+# 010 Feladat: Selenium findby elágazásokkal
+
+# Készíts egy Python alkalmazást ami selenium-ot használ.
+# Indítsd el lokálisan a selenium-py-peldatar alkalmazást.
+# A program töltse be a példatárból az http://localhost:9999/trickyelements.html oldalt.
+# Használj id lokátort és keressd ki az elemenekt egyesével.
+# A legelső olyan elemre ami button típusú kattints rá és ellenőrizd,
+# hogy # a helyes szöveg jelenik-e meg az elemek listája alatt.
+# A megoldást egy elementtype.py nevű fileban kell beadnod.
+
