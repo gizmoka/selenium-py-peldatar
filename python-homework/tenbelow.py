89cee09517d18759d0cbaea5235b2087b75bde8b
Gizella Grunda-Hibaly <gizella.grundahibaly@gmail.com>
2021-06-27 17:19:24 +0200

    accumulation done

--- python-homework/tenbelow.py
+++ python-homework/tenbelow.py
@@ -4,3 +4,12 @@
 # Írj programot, mely addig olvas be számokat a billentyűzetről, ameddig azok kisebbek, mint tíz.
 # Írja ki ezek után a beolvasott számok összegét!
 
+x = 0
+my_number = 0
+while my_number < 10:
+    my_number = int(input("Adj meg egy egész számot! írd ide: "))
+    x = x + my_number
+    if my_number >= 10:
+        break
+print(f"Az eddigi számok összege: {x}")
+# negatív egészre is működik
